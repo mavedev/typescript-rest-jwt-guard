fac9891ddee82fd6207a1f11be4065e4156231c0
Cirill Usatchoff <usatchoff@gmail.com>
2020-12-19 18:06:00 +0200

    Guards for cookies, headers and request body added

--- src/index.ts
+++ src/index.ts
@@ -2,34 +2,171 @@ import { ServiceContext, Errors } from 'typescript-rest';
 import jwt from 'jsonwebtoken';
 
 /**
- * JWT-checking decorator to be used with typescript-rest as annotation.
+ * Get token name and value from string.
+ *
+ * @param str String to retrieve values from.
  */
-export default function JwtGuard(
-  _target: Object,
-  _key: string | symbol,
-  descriptor: PropertyDescriptor
-) {
-  const original = descriptor.value;
+function retrievePair(str: string): string[] {
+  return str.trim().replace(/\s+/g, ' ').split(' ');
+}
 
-  // eslint-disable-next-line
-  descriptor.value = function (...args: any[]) {
-    const context = args.find((arg) => arg instanceof ServiceContext);
+/**
+ * Check if the request context is provided.
+ *
+ * @param context Request context
+ */
+function checkContext(context: ServiceContext | undefined): void {
+  if (!context) {
+    throw new Errors.InternalServerError(
+      'No context provided.'
+    );
+  }
+}
 
-    if (!context) {
-      throw new Errors.InternalServerError('No context provided.');
-    }
+/**
+ * Check if JWT secret is defined.
+ */
+function checkSecret(): void {
+  if (!process.env.SECRET) {
+    throw new Errors.InternalServerError(
+      'Token secret not provided at server.'
+    );
+  }
+}
 
-    const accessToken = (context as ServiceContext).request.cookies['BEARER'];
+/**
+ * JWT-checking decorator to be used with typescript-rest as annotation.
+ * Used when JWT is passed with cookies.
+ */
+export function JwtCookieGuard(tokenName: string) {
+  return (
+    _target: Object,
+    _key: string | symbol,
+    descriptor: PropertyDescriptor
+  ) => {
+    const original = descriptor.value;
 
-    if (!accessToken) {
-      throw new Errors.UnauthorizedError('No access token provided.');
-    }
+    // eslint-disable-next-line
+    descriptor.value = function (...args: any[]) {
+      const context = args.find((arg) => arg instanceof ServiceContext);
 
-    try {
-      jwt.verify(accessToken, process.env.SECRET as string);
-    } catch (error) {
-      throw new Errors.ForbiddenError('Invalid or expired JWT provided.');
-    }
-    return original.apply(this, args);
+      checkContext(context);
+
+      const accessToken = (context as ServiceContext).request.cookies[tokenName];
+
+      if (!accessToken) {
+        throw new Errors.UnauthorizedError(
+          `No '${tokenName}' access token provided.`
+        );
+      }
+
+      checkSecret();
+
+      try {
+        jwt.verify(accessToken, process.env.SECRET as string);
+      } catch (error) {
+        throw new Errors.ForbiddenError(
+          'Invalid or expired JWT provided.'
+        );
+      }
+
+      return original.apply(this, args);
+    };
+  };
+}
+
+/**
+ * JWT-checking decorator to be used with typescript-rest as annotation.
+ * Used when JWT is passed with authorization header.
+ */
+export function JwtHeaderGuard(tokenName: string) {
+  return (
+    _target: Object,
+    _key: string | symbol,
+    descriptor: PropertyDescriptor
+  ) => {
+    const original = descriptor.value;
+
+    // eslint-disable-next-line
+    descriptor.value = function (...args: any[]) {
+      const context = args.find((arg) => arg instanceof ServiceContext);
+
+      checkContext(context);
+
+      const authHeader = (context as ServiceContext).request.headers['authorization'];
+
+      if (!authHeader) {
+        throw new Errors.UnauthorizedError(
+          'No authorization header provided.'
+        );
+      }
+
+      const [headerTokenName, token] = retrievePair(authHeader);
+
+      if (!headerTokenName || !token) {
+        throw new Errors.UnauthorizedError(
+          'Incorrect authorization token value.'
+        );
+      }
+
+      if (headerTokenName !== tokenName) {
+        throw new Errors.UnauthorizedError(
+          `Token name '${headerTokenName}' is not the one that is expected: '${tokenName}'.`
+        );
+      }
+
+      checkSecret();
+
+      try {
+        jwt.verify(token, process.env.SECRET as string);
+      } catch (error) {
+        throw new Errors.ForbiddenError(
+          'Invalid or expired JWT provided.'
+        );
+      }
+
+      return original.apply(this, args);
+    };
+  };
+}
+
+/**
+ * JWT-checking decorator to be used with typescript-rest as annotation.
+ * Used when JWT is passed with request post body data.
+ */
+export function JwtBodyGuard(tokenKey: string) {
+  return (
+    _target: Object,
+    _key: string | symbol,
+    descriptor: PropertyDescriptor
+  ) => {
+    const original = descriptor.value;
+
+    // eslint-disable-next-line
+    descriptor.value = function (...args: any[]) {
+      const context = args.find((arg) => arg instanceof ServiceContext);
+
+      checkContext(context);
+
+      const body = (context as ServiceContext).request.body || {};
+
+      if (!(tokenKey in body)) {
+        throw new Errors.UnauthorizedError(
+          `Request body does not contain '${tokenKey}' key.`
+        );
+      }
+
+      checkSecret();
+
+      try {
+        jwt.verify(body[tokenKey], process.env.SECRET as string);
+      } catch (error) {
+        throw new Errors.ForbiddenError(
+          'Invalid or expired JWT provided.'
+        );
+      }
+
+      return original.apply(this, args);
+    };
   };
 }
